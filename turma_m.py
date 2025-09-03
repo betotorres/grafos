5e6899192657f9cd661eac2f7509132118153d0e
betotorres <betotorres@users.noreply.github.com>
2025-09-03 10:01:12 -0300

    Código Atualizado - Aula Tipos Grafos

--- turma_m.py
+++ turma_m.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 from abc import ABC, abstractmethod
 
 class Grafo(ABC):
@@ -25,6 +26,105 @@ class Grafo(ABC):
     @abstractmethod
     def imprimir(self):
         pass
+
+    #Aula 3 - Atividade 1
+    @abstractmethod
+    def is_simples(self):
+        pass
+
+    @abstractmethod
+    def is_nulo(self):
+        pass
+
+    @abstractmethod
+    def is_completo(self):
+        pass
+
+    #Aula 3 - Atividade 2
+    @abstractmethod
+    def get_vertices(self):
+        pass
+    
+    @abstractmethod
+    def get_arestas(self):
+        pass
+
+    @abstractmethod
+    def is_subgrafo(self, outro_grafo):
+        pass
+
+    @abstractmethod
+    def is_subgrafo_gerador(self, outro_grafo):
+        pass
+
+    @abstractmethod
+    def is_subgrafo_induzido(self, outro_grafo):
+        pass
+
+    #Aula 3 - Atividade 3
+    def _checa_mapeamento_preserva_adjacencia(self, grafo1, grafo2, mapping):
+        """
+        Verifica se uma dada correspondência (mapping) entre vértices preserva
+        a estrutura de adjacência dos grafos.
+        """
+        # Itera sobre todos os pares possíveis de vértices no primeiro grafo
+        arestas_1 = grafo1.get_arestas()
+        arestas_2 = grafo2.get_arestas()
+
+        for aresta in arestas_1:
+            u1, v1 = aresta
+            # Obtém os índices correspondentes no grafo 2
+            u2 = mapping[u1]
+            v2 = mapping[v1]
+
+            # Verifica se a aresta (u1, v1) no grafo 1 tem uma aresta correspondente (u2, v2) no grafo 2
+            if ((u2, v2) not in arestas_2) and ((v2, u2) not in arestas_2):
+                return False
+
+        # Se todos os pares preservam a adjacência, a correspondência é válida.
+        return True
+
+    def is_isomorfo(self, grafo):
+        """
+        Verifica se dois grafos são isomorfos usando força 
+        bruta com poda por invariantes.
+        """
+        # --- ETAPA 1: PODA RÁPIDA COM INVARIANTES ---
+
+        # 1. Invariante: Número de Vértices
+        if (self.numero_de_vertices() != grafo.numero_de_vertices()):
+            return False
+                
+        # 2. Invariante: Número de Arestas
+        if (self.numero_de_arestas() != grafo.numero_de_arestas()):
+            return False
+                
+        # 3. Invariante: Sequência de Graus
+        if self.sequencia_de_graus() != grafo.sequencia_de_graus():
+            return False
+
+        # --- ETAPA 2: BUSCA EXAUSTIVA (FORÇA BRUTA) ---
+
+        # Se os invariantes passaram, devemos testar as correspondências.
+        vertices1 = list(self.get_vertices())
+        vertices2 = list(grafo.get_vertices())
+
+        # Gera todas as permutações dos vértices do grafo 2.
+        #Ex. [1,2] -> (1,2), (2,1)
+        # Cada permutação é uma potencial correspondência para os vértices do grafo 1.
+        for p in itertools.permutations(vertices2):
+            #Cria o mapeamento dos vértices do grafo 1 para o grafo 2
+            #Ex. [A,B] e (1,2), (2,1) -> {A:1, B:2}, {A:2, B:1}
+            mapping = dict(zip(vertices1, p))
+    
+                
+            # Verifica se esta correspondência preserva a estrutura de arestas
+            if self._checa_mapeamento_preserva_adjacencia(self, grafo, mapping):
+                return True
+                    
+            # Se testou todas as permutações e nenhuma funcionou, eles não são isomorfos.
+            return False
+
 
 
 class GrafoDenso(Grafo):
@@ -123,6 +223,107 @@ class GrafoDenso(Grafo):
         print()
 
 
+    #Aula 3 - Atividade 1
+    def is_simples(self):
+        #Percorre a matriz de adjacência para verificar se há laços 
+        for i in range(self.num_vertices):
+            if self.matriz[i][i] != 0:
+                return False
+
+        return True
+
+    def is_nulo(self):
+        if self.numero_de_arestas() == 0 and self.numero_de_vertices() > 0:
+            return True
+        return False
+
+
+    def is_completo(self):
+        if (self.is_simples()) and (
+            self.numero_de_arestas() == (self.numero_de_vertices() *
+                                         (self.numero_de_vertices() - 1)) // 2):
+            return True
+        return False    
+    
+
+    #Aula 3 - Atividade 2
+    def get_vertices(self):
+        return self.labels
+    
+    def get_arestas(self):
+        arestas = []
+        for i in range(self.num_vertices):
+            for j in range(i + 1, self.num_vertices):
+                if self.matriz[i][j] != 0:
+                    arestas.append((self.labels[i], self.labels[j]))
+        return arestas
+
+    def is_subgrafo(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        # Verifica se todos os vértices do grafo atual estão no outro grafo
+        for vertice in self.get_vertices():
+            if vertice not in outro_grafo.get_vertices():
+                return False
+
+        for aresta in self.get_arestas():
+            if aresta not in outro_grafo.get_arestas():
+                return False
+            
+        #Compara se as arestas aparecem na mesma quantidade em ambos os grafos
+        for aresta in self.get_arestas():
+            if self.get_arestas().count(aresta) != outro_grafo.get_arestas().count(aresta):
+                return False
+            
+        # Se todas as arestas e vértices estão presentes, é um subgrafo
+        return True
+    
+
+    def is_subgrafo_gerador(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        if self.is_subgrafo(outro_grafo):
+            if (all(vertice in self.get_vertices() for vertice in outro_grafo.get_vertices())):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def is_subgrafo_induzido(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        if self.is_subgrafo(outro_grafo):
+            lista_arestas = []
+            #Filtra apenas as arestas que conectam os vértices do subgrafo
+            for aresta in outro_grafo.get_arestas():
+                if all(v in self.get_vertices() for v in aresta):
+                    lista_arestas.append(aresta)
+
+            for aresta in self.get_arestas():
+                if aresta not in lista_arestas:
+                    return False
+                
+            for aresta in outro_grafo.get_arestas():
+                if aresta not in self.get_arestas():
+                    return False
+                
+            return True
+        else:
+            return False
+        
+
+
+   
+
+
+
+
+
+
 class GrafoEsparso(Grafo):
     """
     Implementa as operações básicas de um grafo não orientado
@@ -150,14 +351,12 @@ class GrafoEsparso(Grafo):
 
     def numero_de_arestas(self):
         # Retorna o número total de arestas no grafo.
-        return int(sum([len(vizinhos) for vizinhos in self.lista_adj.values()])/2)
+        return (sum([len(vizinhos) for vizinhos in self.lista_adj.values()]) // 2)
 
 
     def sequencia_de_graus(self):
         # Retorna uma lista com os graus de todos os vértices.
         return sorted([len(values) for values in self.lista_adj.values()])
-
-
 
 
     def _validar_vertice(self, vertice):
@@ -210,7 +409,6 @@ class GrafoEsparso(Grafo):
             print(f"Erro ao remover aresta: {e}")
 
 
-
     def imprimir(self):
         """Imprime a lista de adjacências de forma legível."""
         print("\nLista de Adjacências:")
@@ -223,26 +421,144 @@ class GrafoEsparso(Grafo):
             print(f"  {vertice} -> [ {saida} ]")
         print()
 
+    #Atividade 1
+    def is_simples(self):
+        # Verifica se há laços (loops) na lista de adjacências
+        for vertice, vizinhos in self.lista_adj.items():
+            if vertice in vizinhos:
+                return False
+        # Verifica se há arestas duplicadas
+        for vertice, vizinhos in self.lista_adj.items():
+            if len(vizinhos) != len(set(vizinhos)):
+                return False
+        # Se não houver laços e arestas duplicadas, o grafo é simples
+        return True
+
+    def is_nulo(self):
+        if self.numero_de_arestas() == 0 and self.numero_de_vertices() > 0:
+            return True
+        return False
+
+
+    def is_completo(self):
+        if (self.is_simples() and self.numero_de_arestas() == (self.numero_de_vertices() *
+                                         (self.numero_de_vertices() - 1)) // 2):
+            return True
+        return False    
+    
+    #Atividade 2
+    def get_vertices(self):
+        return list(self.lista_adj.keys())
+    
+    def get_arestas(self):
+        arestas = []
+        for vertice, vizinhos in self.lista_adj.items():
+            for vizinho in vizinhos:
+                if (vizinho, vertice) not in arestas:
+                    arestas.append((vertice, vizinho))
+        return arestas
+    
+    def is_subgrafo(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        # Verifica se todos os vértices do grafo atual estão no outro grafo
+        for vertice in self.get_vertices():
+            if vertice not in outro_grafo.get_vertices():
+                return False
+
+        #Verifica se todas as arestas do grafo atual estão no outro grafo
+        for aresta in self.get_arestas():
+            if aresta not in outro_grafo.get_arestas():
+                return False
+            
+        #Compara se as arestas aparecem na mesma quantidade em ambos os grafos
+        for aresta in self.get_arestas():
+            if self.get_arestas().count(aresta) != outro_grafo.get_arestas().count(aresta):
+                return False
+
+        # Se todas as arestas e vértices estão presentes, é um subgrafo
+        return True
+
+    def is_subgrafo_gerador(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        if self.is_subgrafo(outro_grafo):
+            if (all(vertice in self.get_vertices() for vertice in outro_grafo.get_vertices())):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def is_subgrafo_induzido(self, outro_grafo):
+        if not isinstance(outro_grafo, Grafo):
+            raise TypeError("O grafo fornecido deve ser uma instância de Grafo.")
+
+        if self.is_subgrafo(outro_grafo):
+            lista_arestas = []
+            #Filtra apenas as arestas que conectam os vértices do subgrafo
+            for aresta in outro_grafo.get_arestas():
+                if all(v in self.get_vertices() for v in aresta):
+                    lista_arestas.append(aresta)
+
+            for aresta in self.get_arestas():
+                if aresta not in lista_arestas:
+                    return False
+                
+            for aresta in outro_grafo.get_arestas():
+                if aresta not in self.get_arestas():
+                    return False   
+            return True
+        else:
+            return False
+
+
 
 
 if __name__ == "__main__":
     
     vertices_labels = ['A', 'B', 'C', 'D', 'E']
     #g = GrafoDenso(labels=vertices_labels)
-    g = GrafoEsparso(labels=vertices_labels)
+    g = GrafoDenso(labels=vertices_labels)
   
     g.adicionar_aresta('A', 'B')
-    g.adicionar_aresta('A', 'C')
+    g.adicionar_aresta('B', 'C')
     g.adicionar_aresta('A', 'C')
     g.adicionar_aresta('C', 'D')
     g.adicionar_aresta('C', 'E')
-    g.adicionar_aresta('B', 'D')
+
+    vertices_2_labels = ['1', '2', '3', '4', '5']
+    g2 = GrafoEsparso(labels=vertices_2_labels)
+    g2.adicionar_aresta('1', '2')
+    g2.adicionar_aresta('3', '2')
+    g2.adicionar_aresta('1', '3')
+    g2.adicionar_aresta('3', '4')
+    g2.adicionar_aresta('3', '5')
+
+
+    if (g.is_isomorfo(g2)):
+        print("Os grafos são isomorfos.")
+    else:
+        print("Os grafos não são isomorfos.")
+
+    """
     g.imprimir()
     print(f"Número de vértices: {g.numero_de_vertices()}")
     print(f"Número de arestas: {g.numero_de_arestas()}")
     print(f"Sequência de graus: {g.sequencia_de_graus()}")
-    g.remover_aresta('A', 'C')
-    g.imprimir()
+    print(f"É nulo: {g.is_nulo()}")
+    print(f"É completo: {g.is_completo()}")
+    print(f"É simples: {g.is_simples()}")
+    print(f"G2  simples: {g2.is_simples()}")
+    print(f"É subgrafo: {g2.is_subgrafo(g)}")  # Verifica se é subgrafo de si mesmo
+    print(f"É subgrafo gerador: {g2.is_subgrafo_gerador(g)}")  # Verifica se é subgrafo de si mesmo
+    print(f"É subgrafo induzido: {g2.is_subgrafo_induzido(g)}")  # Verifica se é subgrafo de si mesmo
+    print(f"Vértices: {g.get_vertices()}")
+    print(f"Arestas: {g.get_arestas()}")
+
+    """
 
 
     """"
